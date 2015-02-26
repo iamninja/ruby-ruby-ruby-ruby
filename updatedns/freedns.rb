481ad8deacf482d036559aa08cca107e310e0d77
Vagios Vlachos <bagios234@gmail.com>
2015-02-26 04:32:51 +0200

    freedns.org update script

---
+++ updatedns/freedns.rb
@@ -0,0 +1,24 @@
+require 'digest/sha1'
+require 'open-uri'
+require 'rexml/document'
+
+# Credentials
+username = ""
+password = ""
+
+# Generate SHA key
+puts "Generate hash key..."
+hash_key = Digest::SHA1.hexdigest "#{username}|#{password}"
+
+# Make request
+puts "Make request..."
+open("http://freedns.afraid.org/api/?action=getdyndns&sha=#{hash_key}&style=xml") do |r|
+	puts "Status: #{r.status[0]}, #{r.status[1]}."
+	xmlresp = REXML::Document.new(r.read)
+	host = xmlresp.elements['xml/item/host'].text
+	address = xmlresp.elements['xml/item/address'].text
+	puts "#{address} setted on #{host}."
+end
+
+
+
